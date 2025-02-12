fd83d6ee30ecf3fb3a4f96a1c5ad801fdbecfcac
liismaiil <li.1ismai3il@gmail.com>
2025-02-12 08:41:53 +0100

    2025-02-12 sprint window fixed  {to do stars /  valid}

--- app/(dashbord)/hosts/[tokenId]/page.tsx
+++ app/(dashbord)/hosts/[tokenId]/page.tsx
@@ -1,30 +1,26 @@
 'use server'
-import { getHosts } from "@/actions/host";
 import { GuestType } from "@/app/api/graphql/profile/profile.types";
-import HostComponent from "@/components/hosts/Hosts";
-import prisma from "@/lib/prisma-db";
-export async function generateStaticParams() {
-    const hosts = await getHosts();
+import HostComponent from "@/components/hosts/Host";
+import prisma from "@/api/lib/prisma-db";
 
-    return hosts.map((host: GuestType) => ({
-        tokenId: host.tokenId,
-    }))
-}
+
 
 export async function getHost(tokenId: number) {
 
     const guest = await prisma.guest.findUnique({
         where: { tokenId }
     })
+    console.log({guest});
     return guest;
 }
-
+    
 export default async function HostPage({ params }: { params: GuestType }) {
-    const { tokenId } = await params
-
+    const host = await getHost(parseInt(params.tokenId))
+    console.log({host});
+    
     return (
         <div className="flex border-2 h-full w-full p-3  border-violet-500  justify-center items-start gap-3 flex-wrap">
-            <HostComponent tokenId={tokenId} />
+            <HostComponent host={host} />
         </div>
     );
 }
---
+++ components/hosts/Host.tsx
@@ -0,0 +1,42 @@
+'use client'
+import { GuestType } from "@/app/api/graphql/profile/profile.types";
+import {  Pagination } from "@heroui/pagination";
+import {cn} from "@/lib/cn-utility";
+import Image from "next/image";
+import Link from 'next/link';
+import {COUNTRY_CODES} from "@/store/constants/flagArray";
+
+
+
+const HostComponent = ({ host }: { host: GuestType }) => {
+console.log({host});
+//grid grid-cols-4  h-[calc(100vh-7rem)] relative
+return <div className="flex-col  h-screen  w-full p-3   justify-start items-center gap-3 ">
+<div className="flex   w-full p-3   justify-start items-start gap-3 ">
+
+
+<div key={host.tokenId} className='flex-col items-center justify-between shadow-md shadow-blue-400 rounded-md w-32 h-32 gap-2  ' >
+        <Link key={`${host.tokenId}_`} href={`/stages/${host.tokenId}`} scroll={false}>
+        <div className='hover:animate-zoomIn shadow-md cursor-pointer  flex justify-center items-center'>
+                <Image width={70} height={70} className={cn(host.onLine === true && 'border-green-400 border-3  shadow-green-200', ' rounded-full object-cover aspect-square ')} 
+                src={host.flag.startsWith('https://') ? host.flag  : 
+                    `/img/flags/${host.country?.toLowerCase()}.png` } alt={'avatar flag '} />
+            </div>
+            
+        </Link>
+            <div className={cn(host.country !== '' && 'mt-2 p-2 text-center shadow-md')}>
+                {host.country ?? host.country }
+             </div>
+</div>
+</div>
+
+<div className="flex  h-14 w-full p-3   justify-center items-center gap-3 ">
+Products
+{/* <Pagination boundaries={3} color="secondary" total={hosts.length} />
+ */}</div>
+</div>
+
+}
+
+
+export default HostComponent  
